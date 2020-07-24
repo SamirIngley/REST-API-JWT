7120d1210ba71dd001c012113b102c7c161fdeb6
SamirIngley <samir.ingle7@gmail.com>
2020-07-24 12:29:52 -0700

    postman route init

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
+// import routes
+const authRoute = require('./routes/auth')
 
 
-
-
-
-
+// route middlewares (prefixes)
+app.use('/api/user', authRoute);
 
 
 app.listen(3000, () => console.log('Server up and running'));
