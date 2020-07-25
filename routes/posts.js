a674e1084a3cfc3aacaf5bace5250f6d3b074e56
SamirIngley <samir.ingle7@gmail.com>
2020-07-24 21:40:09 -0700

    minor

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const verify = require('./verifyToken');
+const User = require('../models/User');
 
 // CREATING A PRIVATE ROUTE
 router.get('/', verify, (req,res) => {
+    // since we set the token with the id of the user, we can get the user info as long as we provide the token
+    // res.send(req.user) 
+    // User.findOne({_id: req.user})
     res.json({posts: 
         {title: 'SECRET POST', 
         description: 'SECRET DATA // MEMBERS ONLY'
