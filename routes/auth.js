345c89911f02f6957c4603b3d1cbdd4d94f1a5d7
SamirIngley <samir.ingle7@gmail.com>
2020-07-24 19:00:51 -0700

    Validate new user, Check if user email exists, create new user

--- routes/auth.js
+++ routes/auth.js
@@ -6,11 +6,16 @@ const {registerValidation} = require('../validation');
 // validation.js
 router.post('/register', async (req, res) => {  // asyn bc db needs time
 
+    // VALIDATE NEW USER
     const {error} = registerValidation(req.body); // from Joi in validation.js
     if(error) return res.status(400).send(error.details[0].message) // won't continue down to save if error exists
-
     // res.send(error.details[0].message); // sends back your error message!
 
+    // CHECKING IF USER ALREADY EXISTS
+    const emailExists = await User.findOne({email: req.body.email}) // go to user model, check if the email passed in already exists
+    if(emailExists) return res.status(400).send('Email already exists.')
+
+    // CREATE NEW USER
     const user = new User({
         name: req.body.name, // coming from request body - we are creating a user
         email: req.body.email, // but we can't actually read straight from here, we need a bodyParser
