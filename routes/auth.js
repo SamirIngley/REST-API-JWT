768181a726fcd6f6675e3b9f334cf145d68eb1b0
SamirIngley <samir.ingle7@gmail.com>
2020-07-24 21:14:44 -0700

    returns JWT

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const User = require('../models/User');
 const {registerValidation, loginValidation} = require('../validation');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 
 
 // validation.js
@@ -53,7 +55,15 @@ router.post('/login', async (req,res) => {
     const validPass = await bcrypt.compare(req.body.password, user.password);
     if (!validPass) return res.status(400).send('Invalid Password')
     
+    // CREATE AND ASSIGN JWT
+    // we use id bc when we have access to this id, we know user is logged in
+    // other param is some secret we create and add to env in dotenv
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+
+
     res.send('Logged in!')
+
 })
 
 
